39edaa9d8b8201b41be7a34c46ed86ebafbd14a5
George Bora <gpbora@gmail.com>
2019-02-11 22:38:40 +0200

    Added custom dice ability sans remove

--- src/component/app.component.ts
+++ src/component/app.component.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { DiceResult } from '../models/diceResult.model';
 import { CustomNumericDice } from '../models/customNumericDice.model';
 
@@ -8,12 +9,26 @@ import { CustomNumericDice } from '../models/customNumericDice.model';
   selector: 'App',
   templateUrl: 'component/component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   public results: DiceResult[] = [];
   public openCategory: string = 'dnd';
   public customNumberDice: CustomNumericDice[] = [];
+
+  //Modal fields
   public modalOpen: boolean = false;
+  public modalMode: string = 'numeric';
+
+  // Custom numric dice field
+  public custom_num_name: string = 'My D7';
+  public custom_num_min: number = 1;
+  public custom_num_max: number = 7;
+
+  public ngOnInit() {
+    if (localStorage.getItem('custom_numeric_dice')) {
+      this.customNumberDice = JSON.parse(localStorage.getItem('custom_numeric_dice') || '[]');
+    }
+  }
 
   public setCategory(categoryName: string): void {
     this.openCategory = categoryName;
@@ -46,6 +61,22 @@ export class AppComponent {
 
   public createRandomNumeric(): void {
     this.modalOpen = true;
+    this.modalMode = 'numeric';
+    this.custom_num_name = 'My D7';
+    this.custom_num_min = 1;
+    this.custom_num_max = 7;
+  }
+
+  public saveRandomNumeric(): void {
+    this.modalOpen = false;
+    this.customNumberDice.push(new CustomNumericDice(this.custom_num_min, this.custom_num_max, this.custom_num_name));
+    localStorage.setItem('custom_numeric_dice', JSON.stringify(this.customNumberDice));
+  }
+
+  public rollRandomNumeric(index: number): void {
+    let dice: CustomNumericDice = this.customNumberDice[index];
+    let result: number = this.randomNumber(dice.min, dice.max);
+    this.results.unshift(new DiceResult(dice.name, result));
   }
 
   public closeModal(): void {
@@ -54,7 +85,7 @@ export class AppComponent {
 }
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
